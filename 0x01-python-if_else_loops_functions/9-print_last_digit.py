b982591a68787ce2dba64072d8d31715ccc89824
NgBlaze <okolaaprecious@gmail.com>
2023-01-03 06:04:01 -0800

    Created c file

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,16 +1,11 @@
+
 #!/usr/bin/python3
 def print_last_digit(number):
-    if number == 0:
-        print(0, end='')
-        return
-    if number < 0:
-        pos_num = number * 1
-        neg_str = str(pos_num)
-        neg_last_digit_str = neg_str[-1]
-        neg_last_digit_num = int(neg_last_digit_str)
-        print(neg_last_digit_num, end='')
-        return
-    num_str = str(number)
-    last_str = num_str[-1]
-    last_digit = int(last_str)
-    (print(last_digit, end=''))
+    if number >= 0:
+        l_digit = number % 10
+    else:
+        l_digit = number % -10
+        l_digit *= -1
+
+    print("{:d}".format(l_digit), end='')
+    return (l_digit)
